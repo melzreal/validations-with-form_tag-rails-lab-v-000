da0be5b152ed41c67bfcb5031fece410e6121e38
Melissa Real <melissa.real@hotmail.com>
2019-01-09 15:01:10 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :email, uniqueness: true
-  validates(:phone_number, format: { :length => { :is => 10 },  message: "must be 10 characters" }) 
+  validates(:phone_number, format: { :length => { :is => 10 },  message: "must be 10 digits" }) 
 end
